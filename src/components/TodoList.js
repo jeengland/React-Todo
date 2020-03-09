ffb652ca5c9892e6f9aac0bdf469dda50222bcb2
unknown <jacob.e.england@gmail.com>
2020-03-09 16:30:20 -0500

    Added basic styling

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import './TodoList.css';
+
 import Todo from './Todo';
 
 class TodoList extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <h1>Todo List</h1>
                 <ul>
                     {this.props.tasks.map((task) => {
                         return (
